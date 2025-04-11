86a4b84a86bc89bb6fad924d0a3ba25c984f234e
diego <dwigodski@gmail.com>
2025-04-11 09:22:39 -0400

    chore: se arreglaron estilos (font, background, logo) y se eliminaron archivos inncesesarios

--- src/views/App.vue
+++ src/views/App.vue
@@ -29,6 +29,10 @@
     margin: 0px;
   }
 
+  * {
+    font-family: "DM Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" !important;
+  }
+
   @import '../assets/style/app';
 </style>
 
