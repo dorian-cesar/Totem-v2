480259c4d7470cc6011d971a9120fa3235b8cf26
diego <dwigodski@gmail.com>
2025-05-07 12:17:52 -0400

    add: se agregó loading de validación en para server POS en Home

--- src/components/home/BuyPrintInfo.vue
+++ src/components/home/BuyPrintInfo.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="pt-3">
+    <div v-if="loading" class="text-black-50 text-center pb-4">
+      <b-spinner type="grow"></b-spinner>
+      <span class="text-white pl-2 h2">Espere mientras se inicia el servidor</span>
+    </div>
     <b-card class="transparent-main card-custom">
       <blockquote class="card-blockquote">
         <!-- Button Buy Ticket -->
         <div class="btn-img">
-          <b-button variant="link" block href="./travelselection">
+          <b-button
+            variant="link"
+            block
+            :href="serverAvailable ? './travelselection' : null"
+            :disabled="!serverAvailable"
+          >
             <b-img :src="BtnBuyTicket" fluid alt="Buy Ticket" />
           </b-button>
         </div>
         <!-- Button Print Ticket -->
         <div class="btn-img">
-          <b-button variant="link" block href="./print">
+          <b-button variant="link" block :href="serverAvailable ? './print' : null" :disabled="!serverAvailable">
             <b-img :src="BtnPrintTicket" fluid alt="Print Ticket image" />
           </b-button>
         </div>
         <!-- Button Get Ticket -->
         <div class="btn-img">
-          <b-button variant="link" block>
+          <b-button variant="link" block :disabled="!serverAvailable">
             <b-img :src="BtnGetInfo" fluid alt="Get Info" />
           </b-button>
         </div>
@@ -29,14 +38,52 @@
 import BtnBuyTicket from '@/assets/img/buy_print_info/btn_buy_ticket.png'
 import BtnPrintTicket from '@/assets/img/buy_print_info/btn_print_ticket_enabled.png'
 import BtnGetInfo from '@/assets/img/buy_print_info/btn_get_info.png'
+import axios from 'axios'
+import info from '../../../info.json'
 
 export default {
   name: 'BuyPrintInfo',
   data: () => ({
+    loading: true,
+    serverAvailable: false,
     BtnBuyTicket,
     BtnPrintTicket,
-    BtnGetInfo
-  })
+    BtnGetInfo,
+    info,
+    monitorInterval: null
+  }),
+  methods: {
+    checkServerStatus() {
+      axios
+        .get(this.info.urlGetIp)
+        .then((res) => {
+          const monitorIp = res.data.ip
+          console.log('IP del servidor:', monitorIp)
+          
+          return axios.get(`https://${monitorIp}:3000/monitor`)
+        })
+        .then((response) => {
+          console.log('Estado del servidor:', response.data)
+          const isAvailable = response.data.server === true
+          this.serverAvailable = isAvailable
+          this.loading = !isAvailable
+        })
+        .catch((error) => {
+          console.error('Error en el chequeo del estado del servidor:', error)
+          this.serverAvailable = false
+          this.loading = true
+        })
+    }
+  },
+  mounted() {
+    this.checkServerStatus()
+    this.monitorInterval = setInterval(this.checkServerStatus, 10000) // tiempo entre check a server POS
+  },
+  beforeDestroy() {
+    if (this.monitorInterval) {
+      clearInterval(this.monitorInterval)
+    }
+  }
 }
 </script>
 
@@ -49,3 +96,4 @@ export default {
   margin-top: 190px;
 }
 </style>
+
