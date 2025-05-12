cf19384b3efb448a60c40c88415eb0c32c04d3e9
diego <dwigodski@gmail.com>
2025-05-12 12:14:19 -0400

    actualizando cabmios de prod a dev

--- src/components/bus_selection/DinamicBus.vue
+++ src/components/bus_selection/DinamicBus.vue
@@ -163,23 +163,6 @@ export default {
       return floor === 0 ? this.service.servicioPrimerPiso : this.service.servicioSegundoPiso
     },
 
-    // setRef(seatNro) {
-    //   const isSeat = !Number.isNaN(seatNro)
-    //   return isSeat ? `seat-${seatNro}`: ''
-    // },
-
-    // Asignar n si no es número y determinar el número real según el piso
-//     fixNumOfSeatForFloor(num, floor) {
-//       let numNew
-//       if (isNaN(num) || null == num) {
-//         numNew = 'n' //si no es número le asigna n no importa el piso
-//       } else {// si es número verifica el piso
-//         numNew = (1 === floor && !this.isCorrelativo()) // si el número es del segundo piso le suma 20
-//           ? (parseInt(num) + 20).toString()
-//           : num
-//       }
-//       return numNew
-//     },
     getStatusToSeat(num, floor) {
       let status = ''
       if (isNaN(num)) status = 'busy'
@@ -200,12 +183,6 @@ export default {
       }
     },
 
-    // Determinar si el segundo piso tiene números correlativos al primero
-//    isCorrelativo() {
-//      const ultimoPiso1 = this.availableSeats.filter(o => o.floor === 0).length
-//      return ((parseInt(this.availableSeats[ultimoPiso1 - 1].seat) + 1).toString() === this.availableSeats[ultimoPiso1].seat)
-//        ? true : false
-//    },
     // Mensaje de error
     async showMsgBoxError() {
       await this.$bvModal.msgBoxOk('Se presentó un error al reservar el asiento, debe elegir otro.', {
@@ -225,6 +202,7 @@ export default {
 
     //cuando se selecciona un asiento
     async changeStatusSeat({state, num}) {
+      // console.log('params changeStatusSeat', this.param)
       this.seatNum = num
       this.param.asiento = num
 
@@ -258,9 +236,13 @@ export default {
         this.param.origin_id = this.param.origen
         this.param.destination_id = this.param.destino
         this.param.boarding_at = this.param.boarding_at
-        this.param.no_of_seats = 1
+        this.param.no_of_seats = "1"
         this.param.travel_date = this.param.fecha
         this.param.travel_time = this.param.horaSalida
+        this.param.cost
+        this.param.bus_type
+        this.param.route_id
+
         // console.log(this.drawSeats)
         await this.seatReservation('add', this.param, this.param.servicio)
         console.log('DinamicBus: add', this.param)
