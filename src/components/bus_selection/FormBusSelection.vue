daecb97e13fb5ceaa1d66d1f0084b0dcba5488aa
diego <dwigodski@gmail.com>
2025-05-12 12:14:54 -0400

    actualizando cabmios de prod a dev

--- src/components/bus_selection/FormBusSelection.vue
+++ src/components/bus_selection/FormBusSelection.vue
@@ -129,11 +129,11 @@ export default {
       this.propsListCaptionOrigin.totalList = ''
 
       // api dev
-      const proxy = "https://newstg3-gdsbus.kupos.cl"
-      const API_KEY = "TSXFQYAPI25766888"
+      // const proxy = "https://newstg3-gdsbus.kupos.cl"
+      // const API_KEY = "TSXFQYAPI25766888"
       // api kupos
-      // const proxy = "https://gds.kupos.com"
-      // const API_KEY = "TSSDFPAPI30103014"
+      const proxy = "https://gds.kupos.com"
+      const API_KEY = "TSSDFPAPI30103014"
 
       const date = this.changeFormatDate2(this.getDepartureDate(), 'yyyymmdd')
       const api = `/gds/api/ui_schedules/${this.getCodeDepartureCity()}/${this.getCodeArrivalCity()}/${date}.json?api_key=${API_KEY}`
@@ -171,20 +171,11 @@ export default {
       // console.log(results[0])
       // console.log(results[1])
       results.shift();
-//         this.propsListBusDeparture.schedules = results.filter((e) => {
-//           const
-//             [date, month, year ] = e[35].split('-'),
-//             [hour, minutes] = e[9].horaSalida.split(':')
-
-//           const departureTime = new Date(year,month-1,date, hour , minutes)
-
-//           if(departureTime >= validTime){ return  e }
-//         })
 
       this.propsListBusDeparture.schedules = []
       for (let result of results) {
-        // cambiar a 'Pullman Costa'
-        if (result[3] === 'Turbo-kupos-stg1') {
+        // cambiar a 'Turbo-kupos-stg1' para api dev
+        if (result[3] === 'Pullman Costa') {
           let boarding = result[22].split(',')
           let boarding_terminalsText = []
           let boarding_terminalsHTML = []
@@ -197,6 +188,7 @@ export default {
           }
           boarding_terminalsText = boarding_terminalsText.join(' / ')
           boarding_terminalsHTML = boarding_terminalsHTML.join('')
+          console.log("result", result)
           const r = {
             idServicio: result[0],
             operador: result[3],
@@ -223,8 +215,12 @@ export default {
             idClaseBusPisoUno: result[15].split(':')[0],
             idClaseBusPisoDos: result[8].includes('2+1') ? result[15].split(':')[0] : 0,
             ruta: result[2],
+            tipoBus: result[8],
+            costo: result[15],
+            rutaId: result[7]
           }
           this.propsListBusDeparture.schedules.push(r)
+          // console.log("r", r)
         }
       }
       // console.log("propsListBusDeparture.schedules", this.propsListBusDeparture.schedules)
@@ -236,11 +232,11 @@ export default {
       this.propsListCaptionDestination.totalList = ''
 
       // api dev
-      const proxy = "https://newstg3-gdsbus.kupos.cl"
-      const API_KEY = "TSXFQYAPI25766888"
+      // const proxy = "https://newstg3-gdsbus.kupos.cl"
+      // const API_KEY = "TSXFQYAPI25766888"
       // api kupos
-      // const proxy = "https://gds.kupos.com"
-      // const API_KEY = "TSSDFPAPI30103014"
+      const proxy = "https://gds.kupos.com"
+      const API_KEY = "TSSDFPAPI30103014"
       const date = this.changeFormatDate2(this.getReturnDate(), 'yyyymmdd')
       const api = `/gds/api/ui_schedules/${this.getCodeArrivalCity()}/${this.getCodeDepartureCity()}/${date}.json?api_key=${API_KEY}`
       const body = {
@@ -262,8 +258,8 @@ export default {
 
       this.propsListBusDestination.schedules = [];
       for (let result of results) {
-        // cambiar a 'Pullman Costa'
-        if (result[3] === 'Turbo-kupos-stg1') {
+        // cambiar a 'Turbo-kupos-stg1' para api dev
+        if (result[3] === 'Pullman Costa') {
           let boarding = result[22].split(',')
           let boarding_terminalsText = []
           let boarding_terminalsHTML = []
@@ -301,6 +297,9 @@ export default {
             idClaseBusPisoUno: result[15].split(':')[0],
             idClaseBusPisoDos: result[8].includes('2+1') ? result[15].split(':')[0] : 0,
             ruta: result[2],
+            tipoBus: result[8],
+            costo: result[15],
+            rutaId: result[7]
           }
 
           this.propsListBusDestination.schedules.push(r)
