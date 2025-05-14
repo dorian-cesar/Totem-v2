ce2dace6f46f678f8e9b26162fabc155536099aa
diego <dwigodski@gmail.com>
2025-05-14 16:33:32 -0400

    add: se agregó tipo_tarjeta en DB logs en pagoPos y confirm_booking

--- src/components/purchase_detail/FormPurchaseDetail.vue
+++ src/components/purchase_detail/FormPurchaseDetail.vue
@@ -153,6 +153,7 @@ export default {
             estado_boleto: 'Reservado',
             id_pos: '',
             codigo_transaccion: '',
+            tipo_tarjeta: '',
             codigo_autorizacion: '',
             estado_transaccion: '',
             numero_transaccion: '',
@@ -170,6 +171,7 @@ export default {
             const rawTime = this.dataPOS.realTime
             const formattedTime = `${rawTime.slice(0, 2)}:${rawTime.slice(2, 4)}:${rawTime.slice(4, 6)}`
             this.propsPaymentControl.msg = response.data.data.responseMessage
+            bookingData.tipo_tarjeta = this.dataPOS.cardType
             bookingData.codigo_transaccion = this.dataPOS.ticket
             bookingData.codigo_autorizacion = this.dataPOS.authorizationCode
             bookingData.id_pos = this.dataPOS.terminalId
@@ -470,6 +472,7 @@ export default {
                 codigo_transaccion: this.dataPOS.ticket,
                 codigo_autorizacion: this.dataPOS.authorizationCode,
                 id_pos: this.dataPOS.terminalId,
+                tipo_tarjeta: this.dataPOS.cardType,
                 estado_transaccion: 'Pago realizado',
                 numero_transaccion: this.dataPOS.operationNumber,
                 fecha_transaccion: formattedDate,
@@ -508,6 +511,7 @@ export default {
               codigo_transaccion: this.dataPOS.ticket,
               codigo_autorizacion: this.dataPOS.authorizationCode,
               id_pos: this.dataPOS.terminalId,
+              tipo_tarjeta: this.dataPOS.cardType,
               estado_transaccion: 'Pago realizado',
               numero_transaccion: this.dataPOS.operationNumber,
               fecha_transaccion: this.dataPOS.realDate,
