d78c3caefc5eee7e96d3c808e49dea6b5630fff5
diego <dwigodski@gmail.com>
2025-05-14 13:52:01 -0400

    chore: se cambio titulo y placeholder de /print

--- src/components/print/FormPrintTicket.vue
+++ src/components/print/FormPrintTicket.vue
@@ -8,11 +8,11 @@
               <b-row>
                 <b-col class="">
                   <p class="text-primary pb-2 font-weight-bolder s-custom-font">
-                    Ingrese su código de reserva (ej: TS2301150405100000000)
+                    Ingrese su boleto (ej: 1234567)
                   </p>
                   <input
                     type="text"
-                    placeholder="Ingrese su código de reserva"
+                    placeholder="Ingrese su número de boleto"
                     data-layout="normal"
                     class="w-100 input-codigo"
                     style="background-color: azure"
