acad22e0976e71582fb23a296358fffad6fdfeae
diego <dwigodski@gmail.com>
2025-05-14 12:18:08 -0400

    fix: solo guarda numero_boleto en confirm_booking

--- src/components/purchase_detail/FormPurchaseDetail.vue
+++ src/components/purchase_detail/FormPurchaseDetail.vue
@@ -174,7 +174,7 @@ export default {
           hora_viaje: this.propsPersonalInformation.tickets[0].horaSalida,
           asiento: this.propsPersonalInformation.tickets[0].seat,
           codigo_reserva: this.propsPersonalInformation.tickets[0].codeReservation,
-          numero_boleto: this.propsPersonalInformation.tickets[0].operatorPnr,
+          // numero_boleto: this.propsPersonalInformation.tickets[0].operatorPnr,
           estado_boleto: 'Reservado',
           codigo_transaccion: simulatePOSResponse ? this.dataPOS.ticket : '',
           estado_transaccion: simulatePOSResponse ? 'Pago realizado' : 'Pago fallido',
@@ -549,7 +549,7 @@ export default {
             if (typeof data === 'object') {
               let ticket_info = data.result.ticket_details
               let response_boleto = ticket_info.ticket_number
-              let response_codigo = ticket_info.operator_reservation_id
+              let response_codigo = ticket_info.operator_pnr
               let response_servicio = ticket_info.seat_fare_details[0].seat_detail.seat_type
               let response_ruta = ticket_info.service_number
               let response_piso =
@@ -599,7 +599,7 @@ export default {
                 hora_viaje: this.propsPersonalInformation.tickets[0].horaSalida,
                 asiento: response_ticket.asiento,
                 codigo_reserva: response_ticket.boleto,
-                numero_boleto: this.propsPersonalInformation.tickets[0].operatorPnr,
+                numero_boleto: response_ticket.codigo,
                 estado_boleto: 'Confirmado',
                 codigo_confirmacion: '',
                 codigo_transaccion: this.dataPOS.ticket,
@@ -636,7 +636,7 @@ export default {
               hora_viaje: this.propsPersonalInformation.tickets[0].horaSalida,
               asiento: this.propsPersonalInformation.tickets[0].seat,
               codigo_reserva: this.propsPersonalInformation.tickets[0].codeReservation,
-              numero_boleto: this.propsPersonalInformation.tickets[0].operatorPnr,
+              // numero_boleto: this.propsPersonalInformation.tickets[0].operatorPnr,
               estado_boleto: 'Confirmación fallida',
               codigo_confirmacion: '',
               codigo_transaccion: this.dataPOS.ticket,
