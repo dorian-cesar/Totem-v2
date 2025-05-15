16472589c56df04b015fb46542ccf65ad85ea21e
diego <dwigodski@gmail.com>
2025-05-15 12:19:20 -0400

    chore: se agranda select

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -75,9 +75,10 @@
 
 <style scoped>
   .v-select {
-    font-size: 42px;
+    font-size: 52px;
     width: 830px;
     border-radius: 10px;
+    min-height: 85px;
   }
   .v-select .v-text-field.v-text-field--solo .v-input__control { max-height: 18px; }
 
