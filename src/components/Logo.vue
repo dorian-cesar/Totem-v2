e7ef795c45f11693d34d4bb3b83ffab330b13db3
diego <dwigodski@gmail.com>
2025-05-15 13:05:07 -0400

    add: se cambio logo home a blanco

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -10,7 +10,7 @@
 
 <script>
 // Imagen
-import ImgLogo from '@/assets/img/logo-pullman-nuevo.svg'
+import ImgLogo from '@/assets/img/logo-pullman-nuevo-blanco.svg'
 // import ImgLogo from '@/assets/img/logo-pullman-contigo.png'
 
 export default {
