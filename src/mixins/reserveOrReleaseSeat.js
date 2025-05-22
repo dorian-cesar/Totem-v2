3856b4d0811087c0ba3355d2cb72396484b01f91
diego <dwigodski@gmail.com>
2025-05-22 16:52:57 -0400

    add: se agrega correctamente logs en DB de los intentos errados en tentative_booking

--- src/mixins/reserveOrReleaseSeat.js
+++ src/mixins/reserveOrReleaseSeat.js
@@ -19,7 +19,7 @@ export default {
       // const proxy = 'https://newstg3-gdsbus.kupos.cl'
       // const API_KEY = 'TSXFQYAPI25766888'
       // api kupos
-      const proxy = "https://gds.kupos.com"
+      const proxy = 'https://gds.kupos.com'
       const API_KEY = 'TSSDFPAPI30103014'
       let api = ''
 
@@ -71,6 +71,38 @@ export default {
           data = response.data
           success = true
         } catch (error) {
+          const bookingData = {
+            numTotem: localStorage.getItem('ipServer'),
+            rut: localStorage.getItem('rut') || 'empty',
+            origen: this.$store.state.TravelSelection.nameDepartureCity,
+            destino: this.$store.state.TravelSelection.nameArrivalCity,
+            fecha_viaje: param.fecha,
+            hora_viaje: param.horaSalida,
+            asiento: param.book_ticket.seat_details.seat_detail[0].seat_number,
+            codigo_reserva: 'Reserva fallida',
+            // numero_boleto: hasTicketDetails ? data.result.ticket_details.operator_pnr : '',
+            estado_boleto: `Reserva fallida - Intento: ${attempt}`,
+            codigo_autorizacion: '',
+            id_pos: '',
+            tipo_tarjeta: '',
+            tarjeta_marca: '',
+            codigo_transaccion: '',
+            estado_transaccion: 'Pendiente',
+            numero_transaccion: '',
+            fecha_transaccion: '',
+            hora_transaccion: '',
+            total_transaccion: ''
+          }
+          this.axios
+            .post(this.info.urlLogs, {
+              bookingData
+            })
+            .then(() => {
+              console.log(`Intento ${attempt} guardado en DB (tentative booking)`)
+            })
+            .catch((error) => {
+              console.error(`Error al guardar en intento ${attempt} en DB, tentative_booking: `, error)
+            })
           attempt++
           console.warn(`Intento de tentative booking falló, reintentando (${attempt}/${MAX_RETRIES})...`)
           if (attempt >= MAX_RETRIES) {
@@ -78,6 +110,42 @@ export default {
             this.statusReservation = false
             this.codeReservation = ''
             this.isLoadingReservation = false
+            const bookingData = {
+              numTotem: localStorage.getItem('ipServer'),
+              rut: rut || 'empty',
+              origen: this.$store.state.TravelSelection.nameDepartureCity,
+              destino: this.$store.state.TravelSelection.nameArrivalCity,
+              fecha_viaje: param.fecha,
+              hora_viaje: param.horaSalida,
+              asiento: param.book_ticket.seat_details.seat_detail[0].seat_number,
+              codigo_reserva: 'Reserva fallida',
+              // numero_boleto: hasTicketDetails ? data.result.ticket_details.operator_pnr : '',
+              estado_boleto: 'Reserva fallida - Máximo intentos',
+              codigo_autorizacion: '',
+              id_pos: '',
+              tipo_tarjeta: '',
+              tarjeta_marca: '',
+              codigo_transaccion: '',
+              estado_transaccion: 'Pendiente',
+              numero_transaccion: '',
+              fecha_transaccion: '',
+              hora_transaccion: '',
+              total_transaccion: ''
+            }
+            this.axios
+              .post(this.info.urlLogs, {
+                bookingData
+              })
+              .then(() => {
+                console.log('Error guardado en DB (tentative booking)')
+              })
+              .catch((error) => {
+                console.error('Error al guardar en DB, tentative_booking: ', error)
+              })
+            setTimeout(() => {
+              this.$router.push('/travelselection')
+              window.location.reload()
+            }, 3000)
             return
           }
           await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS))
@@ -180,7 +248,6 @@ export default {
           }, 3000)
         }
       }
-
       this.isLoadingReservation = false
     }
   }
