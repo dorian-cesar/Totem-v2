a7c7f73ecbe725bbe325b214cc563e9d1e6dd621
diego <dwigodski@gmail.com>
2025-05-22 12:45:55 -0400

    fix: se fija un maximo de 10 asientos de reserva y compra a cambio de 4 anteriormente

--- src/components/bus_selection/Seat.vue
+++ src/components/bus_selection/Seat.vue
@@ -28,7 +28,7 @@
   import imgBlankSeat from '@/assets/img/seat/blank_seat.png'
   import {mapActions, mapGetters} from 'vuex'
 
-  const SEATS_LIMIT = 4
+  const SEATS_LIMIT = 10
 
   export default {
     name: 'Seat',
