70e022eeac8ca641a6b6c00f10d20a3f79aad0c1
diego <dwigodski@gmail.com>
2025-05-22 15:08:07 -0400

    chore: se cambia limite de asientos en comentario antiguo

--- src/components/bus_selection/Seat.vue
+++ src/components/bus_selection/Seat.vue
@@ -28,7 +28,7 @@
   import imgBlankSeat from '@/assets/img/seat/blank_seat.png'
   import {mapActions, mapGetters} from 'vuex'
 
-// const SEATS_LIMIT = 2
+// const SEATS_LIMIT = 4
   localStorage.setItem('SEATS_LIMIT', 4);
 
   export default {
