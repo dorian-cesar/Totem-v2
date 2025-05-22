acfcdd7dbc4c153d66565e8cf7ca65f0ca19dd0a
diego <dwigodski@gmail.com>
2025-05-22 15:39:50 -0400

    add: se agrega mensaje al usuario en print

--- src/components/print/FormPrintTicket.vue
+++ src/components/print/FormPrintTicket.vue
@@ -29,6 +29,7 @@
               </b-row>
             </b-card-text>
           </b-card>
+          <p class="text-center p-2 mb-0" style="color: azure; font-size: 22px;">Si no tiene el número de su boleto, pregunte al operador(a) con su rut.</p>
           <div :class="keyboardClass"></div>
         </b-col>
       </b-row>
