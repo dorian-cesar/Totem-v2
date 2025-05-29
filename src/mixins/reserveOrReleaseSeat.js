2fbcda0addc9b31d29a2461eb64692243eae1212
diego <dwigodski@gmail.com>
2025-05-29 15:45:05 -0400

    add: se agrega 'sitio' atodos los guardados en DB logs

--- src/mixins/reserveOrReleaseSeat.js
+++ src/mixins/reserveOrReleaseSeat.js
@@ -75,6 +75,7 @@ export default {
           success = true
         } catch (error) {
           const bookingData = {
+            sitio: this.info.sitio,
             numTotem: localStorage.getItem('ipServer'),
             rut: localStorage.getItem('rut') || 'Sin RUT',
             origen: this.$store.state.TravelSelection.nameDepartureCity,
@@ -115,6 +116,7 @@ export default {
             this.codeReservation = ''
             this.isLoadingReservation = false
             const bookingData = {
+              sitio: this.info.sitio,
               numTotem: localStorage.getItem('ipServer'),
               rut: rut || 'Sin RUT',
               origen: this.$store.state.TravelSelection.nameDepartureCity,
