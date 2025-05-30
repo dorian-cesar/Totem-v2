1494ec29a80535d471a0a4bb6a292c9f62f28918
diego <dwigodski@gmail.com>
2025-05-30 13:57:18 -0400

    chore: cambio de version

--- src/components/DateAndTime.vue
+++ src/components/DateAndTime.vue
@@ -6,7 +6,7 @@
         {{ dateTime }}
       </div>
     </div>
-    <p class="version-text mb-0 text-right" style="width: 33%">2.3.0 Version © WIT 2025</p>
+    <p class="version-text mb-0 text-right" style="width: 33%">2.4.0 Version © WIT 2025</p>
   </div>
 </template>
 
