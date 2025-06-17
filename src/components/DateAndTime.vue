1506fb78f107005310c9c524adf75bbb1ddff5ff
diego <dwigodski@gmail.com>
2025-06-17 17:32:42 -0400

    fix: actualizar el número de versión a 2.5.0 en el componente DateAndTime

--- src/components/DateAndTime.vue
+++ src/components/DateAndTime.vue
@@ -6,7 +6,7 @@
         {{ dateTime }}
       </div>
     </div>
-    <p class="version-text mb-0 text-right" style="width: 33%">2.4.0 DEV Version © WIT 2025</p>
+    <p class="version-text mb-0 text-right" style="width: 33%">2.5.0 DEV Version © WIT 2025</p>
   </div>
 </template>
 
