1aafb54b9a278ad5fbb7e12b6410b1975c2bc53f
Mauricio Vega <onlytowork01@gmail.com>
2026-09-07 17:45:31 -0300

    Fix: Screensaver overlay directo desde App.vue sin saltos visuales

--- src/plugins/IdleVue.js
+++ src/plugins/IdleVue.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import idleVue from 'idle-vue'
-import router from '../router'
 
 const eventsHub = new Vue()
 
-// Bandera global de estado
-window.isIdleResetting = false
+// Permitir acceso al eventsHub desde App.vue
+Vue.prototype.$idleEventHub = eventsHub
 
 const options = {
   eventEmitter: eventsHub,
@@ -14,19 +13,3 @@ const options = {
 }
 
 Vue.use(idleVue, options)
-
-eventsHub.$on('idle', () => {
-  const targetHomeName = (typeof IS_STANDBY !== 'undefined' && IS_STANDBY) ? 'Proximamente' : 'Home'
-
-  if (router.currentRoute.name !== targetHomeName && router.currentRoute.path !== '/') {
-    // Activa el escudo de bloqueo
-    window.isIdleResetting = true
-
-    // Redirección directa reemplazando la vista actual en el historial
-    router.replace({ name: targetHomeName }).catch(() => { }).finally(() => {
-      setTimeout(() => {
-        window.isIdleResetting = false
-      }, 500)
-    })
-  }
-})
--- src/views/TravelSelection.vue
+++ src/views/TravelSelection.vue
@@ -12,5 +12,3 @@ export default {
   components: { FormTravelSelection }
 }
 </script>
-
-
