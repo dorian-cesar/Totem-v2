ce7d621b888998de829377e4ef363e7e0986b241
Mauricio Vega <onlytowork01@gmail.com>
2026-09-07 17:09:07 -0300

    Fix: solucion parpadeo por inactividad usando guardia de router

--- src/plugins/IdleVue.js
+++ src/plugins/IdleVue.js
@@ -1,13 +1,32 @@
 import Vue from 'vue'
-import IdleVue from 'idle-vue'
+import idleVue from 'idle-vue'
+import router from '../router'
 
-if (process.env.NODE_ENV !== 'development') {
-  const eventsHub = new Vue()
+const eventsHub = new Vue()
 
-  const options = {
-    eventEmitter: eventsHub,
-    idleTime: (2*60*1000) //<- Tiempo de espera antes de pasar al inicio
-  }
+// Variable global de control
+window.isIdleResetting = false
 
-  Vue.use(IdleVue, options)
+const options = {
+  eventEmitter: eventsHub,
+  idleTime: 30000, // 30 segundos
+  startAtIdle: false
 }
+
+Vue.use(idleVue, options)
+
+eventsHub.$on('idle', () => {
+  const targetHomeName = (typeof IS_STANDBY !== 'undefined' && IS_STANDBY) ? 'Proximamente' : 'Home'
+
+  if (router.currentRoute.name !== targetHomeName && router.currentRoute.path !== '/') {
+    // Activa el escudo de bloqueo de navegación
+    window.isIdleResetting = true
+
+    // Redirige reemplazando la vista para no dejar rastro en el historial
+    router.replace({ name: targetHomeName }).catch(() => { }).finally(() => {
+      setTimeout(() => {
+        window.isIdleResetting = false
+      }, 500)
+    })
+  }
+})
--- src/views/TravelSelection.vue
+++ src/views/TravelSelection.vue
@@ -5,18 +5,12 @@
 </template>
 
 <script>
-  import FormTravelSelection from '@/components/travel_selection/FormTravelSelection'
+import FormTravelSelection from '@/components/travel_selection/FormTravelSelection'
 
-  export default {
-    name: 'TravelSelection',
-    components: {FormTravelSelection},
-    onIdle() {
-      if (this.$router && this.$route && this.$route.name !== 'Home') {
-        this.$router.push({name: 'Home'})
-      }
-    },
-  }
+export default {
+  name: 'TravelSelection',
+  components: { FormTravelSelection }
+}
 </script>
-
 
 
