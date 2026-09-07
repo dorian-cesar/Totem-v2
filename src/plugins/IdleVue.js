3bf5cbf219e4cf5221671a757f22a30b7bc56f5e
Mauricio Vega <onlytowork01@gmail.com>
2026-09-07 17:54:30 -0300

    Fix definitivo: temporizador de inactividad nativo sin router push

--- src/plugins/IdleVue.js
+++ src/plugins/IdleVue.js
@@ -1,15 +1,2 @@
-import Vue from 'vue'
-import idleVue from 'idle-vue'
-
-const eventsHub = new Vue()
-
-// Permitir acceso al eventsHub desde App.vue
-Vue.prototype.$idleEventHub = eventsHub
-
-const options = {
-  eventEmitter: eventsHub,
-  idleTime: 30000, // 30 segundos
-  startAtIdle: false
-}
-
-Vue.use(idleVue, options)
+// Plugin de inactividad deshabilitado a favor del temporizador nativo de App.vue
+export default {}
